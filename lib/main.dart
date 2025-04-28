2ef69e40edca213b98d809cac7da6164d4516034
vira25 <laililmaghfiroh369@gmail.com>
2025-04-28 22:16:49 +0700

    halaman Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:cuanku/pages/home_page.dart';
 
 const String supabaseKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjYWJ1cGp4YnRqZndwYmh5ZGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4OTIwOTgsImV4cCI6MjA2MDQ2ODA5OH0.bJBSjGUqPSezshoeN5D-IGU0dpUT3zskyPhyVPOWdD8';
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> transactions = [
+      {
+        'id': 1,
+        'nama': 'Gaji Bulanan',
+        'tanggal': '2025-04-02',
+        'jumlah': 5000000,
+        'tipe': 'pemasukan',
+      },
+      {
+        'id': 2,
+        'nama': 'Belanja',
+        'tanggal': '2025-04-03',
+        'jumlah': 150000,
+        'tipe': 'pengeluaran',
+      },
+    ];
+
+    int totalIncome = transactions
+        .where((transaction) => transaction['tipe'] == 'pemasukan')
+        .fold(0, (sum, transaction) => sum + (transaction['jumlah'] as int));
+
+    int totalExpense = transactions
+        .where((transaction) => transaction['tipe'] == 'pengeluaran')
+        .fold(0, (sum, transaction) => sum + (transaction['jumlah'] as int));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Uang Lalu lintas'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const Createpage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Total Pemasukan: Rp$totalIncome'),
+              SizedBox(height: 20),
+              Text('Total Pengeluaran: Rp$totalExpense'),
+              SizedBox(height: 20),
+              Expanded(
+                child:
+                    transactions.isEmpty
+                        ? Center(child: Text('Belum ada transaksi'))
+                        : ListView.builder(
+                          itemCount: transactions.length,
+                          itemBuilder: (context, index) {
+                            final isIncome =
+                                transactions[index]['tipe'] == 'pemasukan';
+
+                            return ListTile(
+                              title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(transactions[index]['nama']),
+                                  Text(
+                                    transactions[index]['tanggal'],
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              subtitle: Text(
+                                'Rp${transactions[index]['jumlah']}',
+                              ),
+                              leading: Icon(
+                                isIncome ? Icons.download : Icons.upload,
+                                color: isIncome ? Colors.green : Colors.red,
+                              ),
+                            );
+                          },
+                        ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
