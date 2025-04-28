997910b9728b9a3c91529119890f8898cc3c586c
vira25 <laililmaghfiroh369@gmail.com>
2025-04-28 21:02:18 +0700

    sambungkan supabase sama buat halaman home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+const String supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjYWJ1cGp4YnRqZndwYmh5ZGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4OTIwOTgsImV4cCI6MjA2MDQ2ODA5OH0.bJBSjGUqPSezshoeN5D-IGU0dpUT3zskyPhyVPOWdD8';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://ecabupjxbtjfwpbhydlo.supabase.co',
+    anonKey: supabaseKey,
+  );
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Cuanku yey',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
