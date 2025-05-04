89b4ea84c3e742cedcfe558028ff3afb3322e84f
vira25 <laililmaghfiroh369@gmail.com>
2025-05-04 12:45:41 +0700

    nammbah warna background

--- lib/pages/update_page.dart
+++ lib/pages/update_page.dart
@@ -42,60 +42,65 @@ class _UpdatePageState extends State<UpdatePage> {
     return Scaffold(
       appBar: AppBar(title: const Text('Update Transaksi'), centerTitle: true),
       // Tambahkan konten body di sini jika diperlukan
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Tanggal"),
-                TextField(
-                  controller: tanggalController,
-                  decoration: const InputDecoration(hintText: "YYYY-MM-DD"),
-                ),
-                const SizedBox(height: 20),
-
-                const Text("Nama Transaksi"),
-                TextField(controller: namaController),
-                const SizedBox(height: 20),
-
-                const Text("Tipe Transaksi"),
-                RadioListTile(
-                  title: const Text('pemasukan'),
-                  value: 'pemasukan',
-                  groupValue: tipeTransaksi,
-                  onChanged: (value) {
-                    setState(() {
-                      tipeTransaksi = value.toString();
-                    });
-                  },
-                ),
-                RadioListTile(
-                  title: const Text('pengeluaran'),
-                  value: 'pengeluaran',
-                  groupValue: tipeTransaksi,
-                  onChanged: (value) {
-                    setState(() {
-                      tipeTransaksi = value.toString();
-                    });
-                  },
-                ),
-                const SizedBox(height: 20),
-
-                const Text("Jumlah"),
-                TextField(
-                  controller: jumlahController,
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: updateTransaksi,
-                    child: const Text("Simpan"),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightBlueAccent[50], // warna backgorund
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Tanggal"),
+                  TextField(
+                    controller: tanggalController,
+                    decoration: const InputDecoration(hintText: "YYYY-MM-DD"),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+
+                  const Text("Nama Transaksi"),
+                  TextField(controller: namaController),
+                  const SizedBox(height: 20),
+
+                  const Text("Tipe Transaksi"),
+                  RadioListTile(
+                    title: const Text('pemasukan'),
+                    value: 'pemasukan',
+                    groupValue: tipeTransaksi,
+                    onChanged: (value) {
+                      setState(() {
+                        tipeTransaksi = value.toString();
+                      });
+                    },
+                  ),
+                  RadioListTile(
+                    title: const Text('pengeluaran'),
+                    value: 'pengeluaran',
+                    groupValue: tipeTransaksi,
+                    onChanged: (value) {
+                      setState(() {
+                        tipeTransaksi = value.toString();
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+
+                  const Text("Jumlah"),
+                  TextField(
+                    controller: jumlahController,
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: updateTransaksi,
+                      child: const Text("Simpan"),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
