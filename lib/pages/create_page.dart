f04d88f9d39a26649ba63d2e387260b34c4e9e30
vira25 <laililmaghfiroh369@gmail.com>
2025-05-23 18:48:55 +0700

    nambah validasi dibagian create

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -10,12 +10,18 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
-  final TextEditingController tanggalController = TextEditingController();
-  final TextEditingController namaController = TextEditingController();
-  final TextEditingController jumlahController = TextEditingController();
+  final _formKey = GlobalKey<FormState>(); // untuk validasi semua inputan form
+
+  final TextEditingController tanggalController =
+      TextEditingController(); //mengatur input taggal
+  final TextEditingController namaController =
+      TextEditingController(); //mengatur input nama
+  final TextEditingController jumlahController =
+      TextEditingController(); //mengatur input jumlah
 
   String tipeTransaksi = 'pemasukan';
 
+  //Fungsi untuk membuka date picker dan menyimpan tanggal ke controller
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -34,20 +40,28 @@ class _CreatePageState extends State<CreatePage> {
 
   // untuk menyimpan data transaksi ke database/supabases
   Future<void> _simpanTransaksi() async {
-    try {
-      final response =
-          await Supabase.instance.client.from('uanglalulintas').insert({
-            'tanggal': tanggalController.text,
-            'nama': namaController.text,
-            'tipe': tipeTransaksi,
-            'jumlah': int.parse(jumlahController.text),
-          }).select();
-      if (!mounted) return; // mencegah eror saat menggunakan context
-      if (response.isNotEmpty) {
-        Navigator.pop(context, response.first); // kembali ke halaman sebelumnya
+    if (_formKey.currentState!.validate()) {
+      //untuk mengecek apakah semua inputan sudah benar
+      try {
+        final response =
+            await Supabase.instance.client.from('uanglalulintas').insert({
+              'tanggal': tanggalController.text,
+              'nama': namaController.text,
+              'tipe': tipeTransaksi,
+              'jumlah': int.parse(jumlahController.text),
+            }).select();
+        if (!mounted) return; // mencegah eror saat menggunakan context
+        if (response.isNotEmpty) {
+          Navigator.pop(
+            context,
+            response.first,
+          ); // kembali ke halaman sebelumnya (homePage)
+        }
+      } catch (e) {
+        debugPrint(
+          'Gagal menyimpan transaksi: $e',
+        ); //menampilkan pesan eror jika gagal insert
       }
-    } catch (e) {
-      debugPrint('Gagal menyimpan transaksi: $e'); //menampilkan pesan eror
     }
   }
 
@@ -64,60 +78,86 @@ class _CreatePageState extends State<CreatePage> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Tanggal"),
-                  GestureDetector(
-                    onTap: () => _selectDate(context),
-                    child: AbsorbPointer(
-                      child: TextField(
-                        controller: tanggalController,
-                        decoration: const InputDecoration(
-                          hintText: "YYYY-MM-DD",
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Tanggal"),
+                    GestureDetector(
+                      onTap: () => _selectDate(context),
+                      child: AbsorbPointer(
+                        child: TextFormField(
+                          controller: tanggalController,
+                          decoration: const InputDecoration(
+                            hintText: "YYYY-MM-DD",
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Tanggal tidak boleh kosong';
+                            }
+                            return null;
+                          },
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text("Nama Transaksi"),
-                  TextField(controller: namaController),
-                  const SizedBox(height: 16),
-                  const Text("Tipe Transaksi"),
-                  RadioListTile(
-                    title: const Text('pemasukan'),
-                    value: 'pemasukan',
-                    groupValue: tipeTransaksi,
-                    onChanged: (value) {
-                      setState(() {
-                        tipeTransaksi = value.toString();
-                      });
-                    },
-                  ),
-                  RadioListTile(
-                    title: const Text('pengeluaran'),
-                    value: 'pengeluaran',
-                    groupValue: tipeTransaksi,
-                    onChanged: (value) {
-                      setState(() {
-                        tipeTransaksi = value.toString();
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  const Text("jumlah"),
-                  TextField(
-                    controller: jumlahController,
-                    keyboardType: TextInputType.number,
-                  ),
-                  const SizedBox(height: 16),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: _simpanTransaksi,
-                      child: const Text("Simpan"),
+                    const SizedBox(height: 16),
+                    const Text("Nama Transaksi"),
+                    TextFormField(
+                      controller: namaController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Nama tidak boleh kosong';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 16),
+                    const Text("Tipe Transaksi"),
+                    RadioListTile(
+                      title: const Text('pemasukan'),
+                      value: 'pemasukan',
+                      groupValue: tipeTransaksi,
+                      onChanged: (value) {
+                        setState(() {
+                          tipeTransaksi = value.toString();
+                        });
+                      },
+                    ),
+                    RadioListTile(
+                      title: const Text('pengeluaran'),
+                      value: 'pengeluaran',
+                      groupValue: tipeTransaksi,
+                      onChanged: (value) {
+                        setState(() {
+                          tipeTransaksi = value.toString();
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    const Text("jumlah"),
+                    TextFormField(
+                      controller: jumlahController,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Jumlah tidak boleh kosong';
+                        }
+                        if (int.tryParse(value) == null) {
+                          return 'Jumlah harus berupa angka';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: _simpanTransaksi,
+                        child: const Text("Simpan"),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
